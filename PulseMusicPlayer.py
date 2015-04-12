d2c53993272aaca9d22d64407e1e0a296fed10e8
Antoun Sawires <antounsawires@gmail.com>
2015-04-12 00:38:23 -0400

    Cleaned up code

--- PulseMusicPlayer.py
+++ PulseMusicPlayer.py
@@ -10,22 +10,19 @@ import easygui
 from PIL import Image
 
 
-
+## Tkinter Window | Geometry & Title
 window = tkinter.Tk()
 window.geometry('500x500')
 window.wm_title('Pulse Music Player')
 
 
-
 ## Menu Functions
-def donothing():
+def donothing(): 
 	filewin = TopLevel(window)
 	button = Button(filewin, text = "Do Nothing Button")
 	button.pack
 
 ## Menu Structure for the Music Player
-
-song = filedialog.askopenfilename()
 
 menubar = Menu(window)
 filemenu = Menu(menubar, tearoff=0)
@@ -35,17 +32,16 @@ filemenu.add_command(label="Save", command=donothing)
 filemenu.add_command(label="Save as...", command=donothing)
 filemenu.add_command(label="Close", command=donothing)
 
-
 filemenu.add_command(label="Exit", command=window.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 
 helpmenu = Menu(menubar, tearoff=0)
-
 helpmenu.add_command(label="About...", command=donothing)
 
 
-
 ## Buttons for Playing a stoping the song
+
+song = filedialog.askopenfilename() #Message to open song in program startup
 
 def PlayButton():
 	global g
@@ -58,6 +54,8 @@ def pause():
 def stop():
 	g.stop()
 	
+# #Button Data
+	
 Play = tkinter.Button(window, text = "Play" , command = PlayButton, )
 
 Pause = tkinter.Button(window, text = "Pause or Continue", command = pause)
@@ -65,14 +63,14 @@ Pause = tkinter.Button(window, text = "Pause or Continue", command = pause)
 Stop = tkinter.Button(window, text = "Stop", command = stop)
 
 
-#ButtonData
+# #Canvas & Static Image
 
 Canvas = tkinter.Canvas(window, height=450 , width=350)
 
 img = PhotoImage(file="Empire-pilot.gif")
 
 
-
+# #Everything that needs to be packed and placed is below
 
 Canvas.pack()
 Play.pack()
